4f9c5a766785f657c03ef9434575536543932ff6
arieljuntak <107545690+arieljuntak@users.noreply.github.com>
2022-07-10 06:57:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,15 +31,15 @@ global.github = "https://zeeone-ofc.github.io/" //bebas
 global.email = "zeeoneofc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.owner = ["622150996855","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.owner = ["6288708439726","6282165449262"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6288708439726" // nomor wa kalian
+global.ownernomerr = "+6288708439726" //nmr wa kalian
+global.thumbnail = "./image/Transformers Autobot Logo Poster at AllPosters_com ….jpeg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© Juntakbot-Mdོ' //sticker wm ubah
+global.author = 'Di Buat Oleh JuntakOfc' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
